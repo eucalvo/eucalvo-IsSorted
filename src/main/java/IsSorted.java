b3bc89e204d131e213b0d1deea815f7e650ba2b1
eucalvo <eliamcalvo24@gmail.com>
2023-03-05 17:21:41 +0000

    Completed Lab Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,12 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j])
+                    return false;
+            }
+        }
+        return true;
     }
 }
